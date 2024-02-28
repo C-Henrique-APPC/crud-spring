755a930e83c9a73d49abedf53ddf91d02dc97659
Camilo Henrique <camilo.henrique@pecicero.com.br>
2024-02-28 16:01:00 -0300

    feat: add finbyId

--- src/main/java/com/curso/crudspring/controller/CoursesController.java
+++ src/main/java/com/curso/crudspring/controller/CoursesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,13 @@ public class CoursesController {
     public ResponseEntity<Courses> create(@RequestBody Courses entity) {
         Courses course = this.repository.save(entity);
         return ResponseEntity.status(HttpStatus.CREATED).body(course);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Courses> findById(@PathVariable("id") Long id) {
+        return this.repository.findById(id).map(record -> ResponseEntity.status(HttpStatus.OK).body(record))
+                .orElse(ResponseEntity.notFound().build());
+
     }
 
 }
