e5696a2e432308f3068798fd60446aa926006e23
Camilo Henrique <camilo.henrique@pecicero.com.br>
2024-03-05 10:59:28 -0300

    feat: delete metodo

--- src/main/java/com/curso/crudspring/controller/CoursesController.java
+++ src/main/java/com/curso/crudspring/controller/CoursesController.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,4 +57,12 @@ public class CoursesController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        return repository.findById(id).map(
+                rercordFound -> {
+                    this.repository.delete(rercordFound);
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).<Void>build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
